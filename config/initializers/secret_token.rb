005bd936d3a2cf9a1a587aa77daf27fe06aaf303
Gil Addo <gcaddo4@gcaddo4-VirtualBox.(none)>
2013-10-19 01:21:11 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testproj::Application.config.secret_token = 'cd8b8724503047d7c515e4cbe79eb151e6a4b66109468673c86425ce0a678f7976f503444bd4034b8f01f57cd92867a7cd23266f99a949b1de82b36c0a919b3e'
